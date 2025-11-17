15d732601fb0114e6fb0b2b692325ddcc155b8fd
ovirandunu <133049040+ovirandunu@users.noreply.github.com>
2025-11-18 00:50:34 +0100

    Fix return statement in hello_world function

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello Nigga Vihanga++Noah++Demir++isGay=true!!!"
+    return "Hello Nigga Vihanga++Noah++Demir++isGay=true!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
