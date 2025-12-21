aec87819b4f5fc0ded73ec20b4086342634b6f58
ovirandunu <133049040+ovirandunu@users.noreply.github.com>
2025-12-21 05:30:50 +0100

    Update header from v1 to v2

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def hello_world():
     </head>
     <body>
         <div class="container">
-            <h1>Migara-Gay-nigga-v1</h1>
+            <h1>Migara-Gay-nigga-v2</h1>
             
             <div class="section">
                 <div class="label">Environment Variable (TEST_ENV_VAR):</div>
